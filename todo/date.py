28904daa8efbd59655bff9b76d467116da03da45
Dylan Hwang <dylanhwang@berkeley.edu>
2019-03-08 21:13:53 -0800

    added to entry

--- todo/date.py
+++ todo/date.py
@@ -1,10 +1,20 @@
+import json 
+
 class Date:
     """
     """
-    def Date(self, year, month, day, time):
-        pass
+    def __init__(self, year, month, day, time): 
+        self.year = year 
+        self.month = month 
+        self.day = day 
+        self.time = time 
 
     def __lt__(other_date):
         pass
 
+    def json_out(self):
+        """ 
+        creeate 
+        """
+        return self.__dict__
 
