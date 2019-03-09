4ae029bd3cdf2b472a2086e61d45422311df8d10
Tae Hyung Kim <thkim1011@berkeley.edu>
2019-03-08 20:17:28 -0800

    new changes

--- todo/entry.py
+++ todo/entry.py
@@ -1,3 +1,12 @@
+import 
+
+def json_to_entry(json_str):
+    """
+    Converts json string to an Entry object.
+    """
+    pass
+
+
 class Entry:
     """
     Entry class.
@@ -15,4 +24,16 @@ class Entry:
         self.deadline = deadline
         self.priority = priority
 
+    def __str__(self):
+        pass
+
+    def json_out(self):
+        """
+        Decompresses each object into json object. 
+        >>> date = Date(2019, 3, 8, 10, 0) # 3/8/19 at 10:00
+        >>> entry = Entry("Math Homework", date, 2)
+        >>> entry.json_out()
+        {'description': 'Math Homework', 'deadline': ... }
+        """
+        pass
 
