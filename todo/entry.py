3efeab1c1f4f6a8f4177826e00fc995eb792da6f
Tae Hyung Kim <thkim1011@berkeley.edu>
2019-03-09 18:03:54 -0800

    fixed add_entry

--- todo/entry.py
+++ todo/entry.py
@@ -13,7 +13,7 @@ class Entry:
     Entry class.
     """
 
-    def __init__(self, description,priority, date_created, deadline = None, completed = False):
+    def __init__(self, description, priority, deadline=None, completed=False):
         """
         Constructs a new entry for the Todo List.
         description -- Description of the task to be finished 
@@ -23,9 +23,7 @@ class Entry:
         self.description = description
         self.deadline = deadline
         self.priority = priority
-        self.priority = priority 
         self.completed = completed 
-        self.date_created = date_created
 
     def __str__(self):
         pass
