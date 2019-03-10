a10bd208774177d28006b0c36e1ea1cbf2b4e9ef
Dylan Hwang <dylanhwang@berkeley.edu>
2019-03-10 01:29:40 -0800

    add curses functionality

--- todo/__main__.py
+++ todo/__main__.py
@@ -10,6 +10,9 @@ from todo.date import Date
 import datetime
 from collections import OrderedDict
 
+import curses
+import curses.textpad
+
 priority0 = []
 priority1 = [] 
 priority2 = [] 
@@ -24,6 +27,7 @@ def print_help():
         "help": "Prints out the help text.",
         "add": "Adds a new entry.",
         "list": "Lists out all the todo entries.",
+        "guil": "List out all the todo entries in GUI.",
         "edit": "Edit an entry.",
     }
     help_dict = OrderedDict(sorted(help_dict.items()))
@@ -67,25 +71,36 @@ def list_entries():
     """
     Lists all entries
     """
-
+    global priority0 
+    global priority1 
+    global priority2 
     home = os.path.expanduser("~")
 
     with open(home + "/.todo.json") as f:
         data = json.load(f)
+        print(data)
         for entry in data: 
+            print(entry)
             if entry["priority"] == 0:
                 priority0.append(entry)
             elif entry["priority"] == 1:
                 priority1.append(entry)
             else:
                 priority2.append(entry)
-    priorities = ["HIGH PRIORITY", "MEDIUM PRIORITY", "LOW PRIORITY"]
-    for iter in range(3):
-        print(priorities[iter])
-        for index, entry in enumerate(priority1, 1):
+    priorities = ["LOW PRIORITY", "MEDIUM PRIORITY","HIGH PRIORITY"]
+    for iter in range(2, -1, -1):
+        print("PRIORITY get", priorities[iter])
+        priority = None 
+        if iter == 0: 
+            priority = priority0 
+        elif iter == 1: 
+            priority = priority1 
+        else: 
+            priority = priority2 
+        for index, entry in enumerate(priority, 1):
             deadline = entry["deadline"] 
-            print(index + ".", str(deadline["month"] + "/" str(deadline["day"] + "/" + \
-                str(deadline["year"]))
+            print(str(index) + ".", str(deadline["month"]) + "/"  + str(deadline["day"]) + "/" + str(deadline["year"]))
+            print('\t', entry["description"])
 
 def edit_entry():
     """
@@ -109,6 +124,148 @@ def mark_complete(entry):
     """
     pass
 
+def draw_menu(stdscr):
+    """
+    Lists the entries similar to list_entries. This lists it out in 
+    a GUI that is generated using the Curses package with python 
+    """
+    global priority0 
+    global priority1 
+    global priority2 
+    k = 0
+    cursor_x = 0
+    cursor_y = 0
+
+    # Clear and refresh the screen for a blank canvas
+    stdscr.clear()
+    stdscr.refresh()
+
+    # Start colors in curses
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    #add entries into list to be output later 
+    home = os.path.expanduser("~")
+    with open(home + "/.todo.json") as f:
+        data = json.load(f)
+        print(data)
+        for entry in data: 
+            print(entry)
+            if entry["priority"] == "0":
+                priority0.append(entry)
+            elif entry["priority"] == "1":
+                priority1.append(entry)
+            else:
+                priority2.append(entry)
+
+    # Loop where k is the last character pressed
+    while (k != ord('q')):
+
+        # Initialization
+        stdscr.clear()
+        height, width = stdscr.getmaxyx()
+
+        if k == ord('j'):
+            cursor_y = cursor_y + 1
+        elif k == ord('k'):
+            cursor_y = cursor_y - 1
+        elif k == ord('l'):
+            cursor_x = cursor_x + 1
+        elif k == ord('h'):
+            cursor_x = cursor_x - 1
+
+        cursor_x = max(0, cursor_x)
+        cursor_x = min(width-1, cursor_x)
+
+        cursor_y = max(0, cursor_y)
+        cursor_y = min(height-1, cursor_y)
+
+        # Declaration of strings
+        title = "Curses example"[:width-1]
+        subtitle = "Written by Clay McLeod"[:width-1]
+        keystr = "Last key pressed: {}".format(k)[:width-1]
+        statusbarstr = "Press 'q' to exit | Todo GUI | Pos: {}, {}".format(cursor_x, cursor_y)
+        if k == 0:
+            keystr = "No key press detected..."[:width-1]
+
+        start_list_y = 2
+        priorities = ["LOW PRIORITY", "MEDIUM PRIORITY","HIGH PRIORITY"]
+        for iter in range(2, -1, -1):
+            priority_out = priorities[iter]
+            boarder = "=" * (width - 1)
+            stdscr.addstr(start_list_y, 1, boarder)
+            start_list_y += 1
+            stdscr.addstr(start_list_y, 1, priority_out)
+            start_list_y += 1
+            stdscr.addstr(start_list_y, 1, boarder)
+            start_list_y += 1
+            priority = None 
+            if iter == 0: 
+                priority = priority0 
+            elif iter == 1: 
+                priority = priority1 
+            else: 
+                priority = priority2 
+            for index, entry in enumerate(priority, 1):
+                deadline = entry["deadline"] 
+                content = str(index) + ".  " +  str(deadline["month"]) + "/"  + str(deadline["day"]) + "/" + str(deadline["year"])
+                stdscr.addstr(start_list_y, 3, content)
+                start_list_y += 1
+                stdscr.addstr(start_list_y, 5, entry["description"])
+                start_list_y += 2
+
+        # win = curses.newwin(5, 60, 5, 10)
+
+        # tb = curses.textpad.Textbox(win, insert_mode=True)
+        # text = tb.edit()
+        # curses.flash()
+        # win.clear()
+        # win.addstr(0, 0, text.encode('utf-8'))
+        # win.refresh()
+        # win.getch()
+
+        # Centering calculations
+        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
+        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
+        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
+        start_y = int((height // 2) - 2)
+
+        # Rendering some text
+        # each curly brace represents one variable inside format method
+        whstr = "Width: {}, Height: {}".format(width, height)
+        stdscr.addstr(0, 0, whstr, curses.color_pair(1))
+
+        # Render status bar
+        stdscr.attron(curses.color_pair(3))
+        stdscr.addstr(height-1, 0, statusbarstr)
+        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+        stdscr.attroff(curses.color_pair(3))
+
+        # Turning on attributes for title
+        stdscr.attron(curses.color_pair(2))
+        stdscr.attron(curses.A_BOLD)
+
+        # Rendering title
+        stdscr.addstr(start_y, start_x_title, title)
+
+        # Turning off attributes for title
+        stdscr.attroff(curses.color_pair(2))
+        stdscr.attroff(curses.A_BOLD)
+
+        # Print rest of text
+        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
+        stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
+        stdscr.addstr(start_y + 5, start_x_keystr, keystr)
+        stdscr.move(cursor_y, cursor_x)
+
+        # Refresh the screen
+        stdscr.refresh()
+
+        # Wait for next input
+        k = stdscr.getch()
+
 def main():
     """
     Main function for program. 
@@ -123,6 +280,10 @@ def main():
         add_entry()
     if command == "edit":
         edit_entry()
+    if command == "list":
+        list_entries()
+    if command == "guil":
+        curses.wrapper(draw_menu)
 
 if __name__ == "__main__":
     main()
